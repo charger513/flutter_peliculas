f0fd036843c825b03727e2d12edd0f95d4adcbb8
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-16 17:31:30 -0600

    Películas provider

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_peliculas/src/providers/peliculas_provider.dart';
 import 'package:flutter_peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -27,6 +28,9 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
+    final peliculasProvider = new PeliculasProvider();
+    peliculasProvider.getEnCines();
+
     return CardSwiper(
       peliculas: [1,2,3,4,5]
     );
