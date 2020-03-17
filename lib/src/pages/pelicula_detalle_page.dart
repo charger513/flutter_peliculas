ad1fb5a0d3abfcad0e2be053e82b28f7ae2b0002
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-17 14:12:10 -0600

    Hero Animation

--- lib/src/pages/pelicula_detalle_page.dart
+++ lib/src/pages/pelicula_detalle_page.dart
@@ -61,11 +61,14 @@ class PeliculaDetallePage extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Row(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image(
-              image: NetworkImage(pelicula.getPosterImg()),
-              height: 150.0,
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage(pelicula.getPosterImg()),
+                height: 150.0,
+              ),
             ),
           ),
           SizedBox(width: 20.0),
