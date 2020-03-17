39e613f1615c6342081e9e79ddc6df14b6622c48
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-17 10:27:38 -0600

    Creando un Stream de películas

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter_peliculas/src/models/pelicula_model.dart';
 
@@ -8,18 +9,40 @@ class PeliculasProvider {
   String _url = 'api.themoviedb.org';
   String _language = 'es-MX';
 
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
+
   Future<List<Pelicula>> getEnCines() {
     return _get('3/movie/now_playing');
   }
 
-  Future<List<Pelicula>> getPopulares() {
-    return _get('3/movie/popular');
+  Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
+
+    final resp = await _get('3/movie/popular', page: _popularesPage);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+
+    return resp;
   }
 
-  Future<List<Pelicula>> _get(String path) async {
+  Future<List<Pelicula>> _get(String path, {int page = 1}) async {
     final url = Uri.https(_url, path, {
       'api_key': _apikey,
-      'language': _language
+      'language': _language,
+      'page': page.toString()
     });
 
     final resp = await http.get(url);
