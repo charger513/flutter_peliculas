2a3a2df7190bb335e8704304644784ad4ca2ebdf
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-17 10:55:16 -0600

    Streambuilder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -65,11 +65,11 @@ class HomePage extends StatelessWidget {
             child: Text('Populares', style: Theme.of(context).textTheme.subhead)
           ),
           SizedBox(height: 5.0,),
-          FutureBuilder(
-            future: peliculasProvider.getPopulares(),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
               if(snapshot.hasData) {
-                return MovieHorizontal(peliculas: snapshot.data);
+                return MovieHorizontal(peliculas: snapshot.data, siguientePagina: peliculasProvider.getPopulares);
               } else {
                 return Center(child: CircularProgressIndicator());
               }
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -5,21 +5,31 @@ class MovieHorizontal extends StatelessWidget {
 
   final List<Pelicula> peliculas;
 
-  MovieHorizontal({ @required this.peliculas });
+  final Function siguientePagina;
+
+  MovieHorizontal({ @required this.peliculas, @required this.siguientePagina });
+
+  final _pageController = new PageController(
+    initialPage: 1,
+    viewportFraction: 0.3
+  );
 
   @override
   Widget build(BuildContext context) {
 
     final _screenSize = MediaQuery.of(context).size;
 
+    _pageController.addListener(() {
+      if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200) {
+        siguientePagina();
+      }
+    });
+
     return Container(
       height: _screenSize.height * 0.25,
       child: PageView(
         children: _tarjetas(context),
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3
-        ),
+        controller: _pageController,
         pageSnapping: false,
       ),
     );
